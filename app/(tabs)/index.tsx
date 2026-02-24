898276d1236080bf854c6665a79472fb48f5f8e3
Gabe Hatler <gabe.hatler@gmail.com>
2026-02-24 12:53:52 -0700

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+      <ThemedText type="title">Welcome Gabe and Sam! 🚀🔥</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
